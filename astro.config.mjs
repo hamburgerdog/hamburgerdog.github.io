c4c37e6c66898c401e0cb91500a377ff76305864
hamburgerdog <838980000@qq.com>
2023-11-30 17:12:47 +0800

    perf: add image alt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import preact from '@astrojs/preact';
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://hamburgerdog.github.io',
-	integrations: [preact()],
 });

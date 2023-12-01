bad6512f470f815bd0f8604f7d0a93a62a6b021f
hamburgerdog <838980000@qq.com>
2023-12-01 12:04:31 +0800

    feat: add toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,22 @@
 import { defineConfig } from 'astro/config';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import remarkToc from 'remark-toc';
+
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://hamburgerdog.github.io',
+	markdown: {
+		// 应用于 .md 和 .mdx 文件
+		remarkPlugins: [[remarkToc, { heading: 'contents' }]],
+		rehypePlugins: [
+			rehypeAccessibleEmojis,
+			rehypeSlug,
+			rehypeHeadingIds,
+			[rehypeAutolinkHeadings, { behavior: 'append' }],
+		],
+	},
 });
